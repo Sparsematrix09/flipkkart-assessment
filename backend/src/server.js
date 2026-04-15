3bb12e14161275b93d64dc51c010030aa82bfa0c
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2026-04-15 17:09:59 +0530

    fix: Correct database import path in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { QueryTypes } = require('sequelize');
-const sequelize = require('./db');
+const sequelize = require('./db/index.js');
 
 dotenv.config();
 
