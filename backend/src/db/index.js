25890db59cfa4ff30e18bc83178537c4ae176b8f
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2026-04-15 16:11:15 +0530

    Prepare for production deployment

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+//neon connection string
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  
+    }
+  }
 });
 
 module.exports = sequelize;
